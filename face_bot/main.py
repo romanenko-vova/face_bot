877823ffca02aea25f0478dd7284303509f707b1
romanenko_vova <bonyshd@gmail.com>
2024-07-31 23:28:56 +0300

    base structure

--- face_bot/main.py
+++ face_bot/main.py
@@ -2,10 +2,12 @@ import logging
 import os
 
 from dotenv import load_dotenv
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ConversationHandler
+
+from handlers.handlers import start
 
 load_dotenv()
+
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -16,13 +18,17 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-
-if __name__ == '__main__':
+def main():
     application = ApplicationBuilder().token(os.getenv('TOKEN')).build()
     
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={},
+        fallbacks=[]
+    )
+    application.add_handler(conv_handler)
     
     application.run_polling()
+
+if __name__ == '__main__':
+    main()
